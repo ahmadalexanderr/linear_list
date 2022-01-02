85220ab7e6a858b0ef822e8ac059ac9f1cafb519
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-03 02:51:00 +0700

    Archieve Strukdat List Linear

---
+++ CMakeFiles/24010316140048.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "24010316140048"
+  "24010316140048.pdb"
+  "CMakeFiles/24010316140048.dir/SLLNC.c.o"
+  "CMakeFiles/24010316140048.dir/mSLLNC.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/24010316140048.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
